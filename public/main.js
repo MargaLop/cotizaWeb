fff4d8ac49aefa40815a0de549f1d1d7f2eb83db
Marga <margalppalacios@gmail.com>
2022-07-02 12:14:33 +0200

    cotización cada 15 segundos

--- public/main.js
+++ public/main.js
@@ -1,16 +1,25 @@
-var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-  
-  fetch("https://api.coinbase.com/v2/exchange-rates?currency=BTC", requestOptions)
-    .then((response) => response.text())
-    .then((result) => {
-      const rate = document.getElementById("rate");
-      resultado=JSON.parse(result);
-      monedaTxtEur = resultado['data']['rates']['EUR']
-      monedaFormateadaEur = parseFloat(monedaTxtEur).toFixed(2)
-      rate.textContent  = `1 = ${monedaFormateadaEur}`
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function main(){
+  var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+    while(true){
+      fetch("https://api.coinbase.com/v2/exchange-rates?currency=BTC", requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+          const rate = document.getElementById("rate");
+          resultado=JSON.parse(result);
+          monedaTxtEur = resultado['data']['rates']['EUR']
+          monedaFormateadaEur = parseFloat(monedaTxtEur).toFixed(2)
+          rate.textContent  = `1 = ${monedaFormateadaEur}`
 
-    })
-    .catch((error) => console.log("error", error));
+          console.log(monedaTxtEur)
+        })
+        .catch((error) => console.log("error", error));
+        await sleep(15 * 1000);
+    }
+}
+main()
